542b97a8585516657d6ff15a39be9e5b0755803a
melekpro <melekpro6@gmail.com>
2024-04-11 05:30:55 -0700

    Update NUMBTOHEX.bat

--- NUMBTOHEX.bat
+++ NUMBTOHEX.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p number=Enter a number: 
-echo Greetings, Daniel! Here is the hexadecimal representation of %number%: 0x%number%
+echo Greetings, melek! Here is the hexadecimal representation of %number%: 0x%number%
 pause >nul
